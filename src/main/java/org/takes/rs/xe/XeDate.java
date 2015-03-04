6e63eb3cc61c2b701a3d03bd3ed84fd1451101ab
Yegor Bugayenko <yegor@tpc2.com>
2015-03-03 22:19:33 -0800

    #5 XeDate

---
+++ src/main/java/org/takes/rs/xe/XeDate.java
@@ -0,0 +1,75 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.takes.rs.xe;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+import lombok.EqualsAndHashCode;
+import org.xembly.Directive;
+import org.xembly.Directives;
+
+/**
+ * Xembly source to create SLA attribute with current date/time in ISO 8601.
+ *
+ * @author Yegor Bugayenko (yegor@teamed.io)
+ * @version $Id$
+ * @since 0.3
+ */
+@EqualsAndHashCode
+public final class XeDate implements XeSource {
+
+    /**
+     * Attribute name.
+     */
+    private final transient String attr;
+
+    /**
+     * Ctor.
+     */
+    public XeDate() {
+        this("date");
+    }
+
+    /**
+     * Ctor.
+     * @param name Attribute name
+     */
+    public XeDate(final String name) {
+        this.attr = name;
+    }
+
+    @Override
+    public Iterable<Directive> toXembly() {
+        final DateFormat fmt = new SimpleDateFormat(
+            "yyyy-MM-dd'T'HH:mmZ", Locale.ENGLISH
+        );
+        fmt.setTimeZone(TimeZone.getTimeZone("UTC"));
+        return new Directives().attr(
+            this.attr, fmt.format(new Date())
+        );
+    }
+}
