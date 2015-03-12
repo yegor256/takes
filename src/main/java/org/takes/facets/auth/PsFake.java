09e703f5be23ba13d5dfbfda212b78037c1648f2
Yegor Bugayenko <yegor@tpc2.com>
2015-03-12 16:53:59 -0700

    #54 PsFake

---
+++ src/main/java/org/takes/facets/auth/PsFake.java
@@ -0,0 +1,70 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.takes.facets.auth;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import lombok.EqualsAndHashCode;
+import org.takes.Request;
+import org.takes.Response;
+
+/**
+ * Fake pass.
+ *
+ * <p>The class is immutable and thread-safe.
+ *
+ * @author Yegor Bugayenko (yegor@teamed.io)
+ * @version $Id$
+ * @since 0.9
+ */
+@EqualsAndHashCode(of = "condition")
+public final class PsFake implements Pass {
+
+    /**
+     * Should we authenticate a user?
+     */
+    private final transient boolean condition;
+
+    /**
+     * Ctor.
+     * @param cond Condition
+     */
+    public PsFake(final boolean cond) {
+        this.condition = cond;
+    }
+
+    @Override
+    public Collection<Identity> enter(final Request request) {
+        final Collection<Identity> users = new ArrayList<Identity>(1);
+        if (this.condition) {
+            users.add(new Identity.Simple("urn:test:1"));
+        }
+        return users;
+    }
+
+    @Override
+    public Response exit(final Response response, final Identity identity) {
+        return response;
+    }
+}
