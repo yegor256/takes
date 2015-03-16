7cce4f92b7c36ce65a5dd193f7deeab7f79066d6
Yegor Bugayenko <yegor@tpc2.com>
2015-03-16 15:24:50 -0700

    #60 PsBugFlagTest

---
+++ src/test/java/org/takes/facets/auth/PsByFlagTest.java
@@ -0,0 +1,56 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.takes.facets.auth;
+
+import java.io.IOException;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.takes.rq.RqFake;
+
+/**
+ * Test case for {@link PsByFlag}.
+ * @author Yegor Bugayenko (yegor@teamed.io)
+ * @version $Id$
+ * @since 0.10
+ */
+public final class PsByFlagTest {
+
+    /**
+     * PsByFlag can skip if nothing found.
+     * @throws IOException If some problem inside
+     */
+    @Test
+    public void skipsIfNothingFound() throws IOException {
+        MatcherAssert.assertThat(
+            new PsByFlag(
+                new PsByFlag.Pair(
+                    "some-key", new PsFake(true)
+                )
+            ).enter(new RqFake("GET", "/?PsByFlag=x")).hasNext(),
+            Matchers.is(false)
+        );
+    }
+
+}
