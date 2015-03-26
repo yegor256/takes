b52022b20ac8fdf167456eef303711b3d7f52f3b
Dmitry Zaytsev <dmzaytsev@users.noreply.github.com>
2015-03-26 19:00:04 +0500

    #47 added logging level to cookie added unit test

--- src/main/java/org/takes/facets/flash/RsFlash.java
+++ src/main/java/org/takes/facets/flash/RsFlash.java
@@ -34,6 +34,7 @@ import org.takes.rs.RsWrap;
  * Forwarding response.
  *
  * <p>The class is immutable and thread-safe.
+ *
  * @author Yegor Bugayenko (yegor@teamed.io)
  * @version $Id$
  * @since 0.1
