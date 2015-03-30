a6ef75eb1016aaf6b543423f548c28ffac3161ea
Igor Khvostenkov <ikhvostenkov@gmail.com>
2015-03-30 21:14:43 +0200

    CcBase64 #19 - CcBase64 class and CcBase64Test have been created

---
+++ src/main/java/org/takes/facets/auth/codecs/CcBase64.java
@@ -0,0 +1,67 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.takes.facets.auth.codecs;
+
+import java.io.IOException;
+import lombok.EqualsAndHashCode;
+import org.apache.commons.codec.binary.Base64;
+import org.takes.facets.auth.Identity;
+
+/**
+ * Base64 codec.
+ *
+ * <p>The class is immutable and thread-safe.
+ *
+ * @author Igor Khvostenkov (ikhvostenkov@gmail.com)
+ * @version $Id$
+ * @since 0.11
+ */
+@EqualsAndHashCode
+public final class CcBase64 implements Codec {
+    /**
+     * Original codec.
+     */
+    private final transient Codec origin;
+
+    /**
+     * Ctor.
+     *
+     * @param codec Original codec
+     */
+    public CcBase64(final Codec codec) {
+        this.origin = codec;
+    }
+
+    @Override
+    public byte[] encode(final Identity identity) throws IOException {
+        final byte[] raw = this.origin.encode(identity);
+        return Base64.encodeBase64(raw);
+    }
+
+    @Override
+    public Identity decode(final byte[] bytes) throws IOException {
+        final byte[] raw = Base64.decodeBase64(bytes);
+        return this.origin.decode(raw);
+    }
+}
---
+++ src/test/java/org/takes/facets/auth/codecs/CcBase64Test.java
@@ -0,0 +1,123 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.takes.facets.auth.codecs;
+
+import com.google.common.collect.ImmutableMap;
+import java.io.IOException;
+import java.util.Map;
+import nl.jqno.equalsverifier.EqualsVerifier;
+import nl.jqno.equalsverifier.Warning;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.takes.facets.auth.Identity;
+
+/**
+ * Test case for {@link CcBase64}.
+ *
+ * @author Igor Khvostenkov (ikhvostenkov@gmail.com)
+ * @version $Id$
+ * @since 0.11
+ */
+public final class CcBase64Test {
+
+    /**
+     * CcBase64 can encode.
+     *
+     * @throws IOException If some problem inside
+     */
+    @Test
+    public void encodes() throws IOException {
+        final Identity identity = new Identity.Simple("urn:test:3");
+        MatcherAssert.assertThat(
+            new String(new CcBase64(new CcPlain()).encode(identity)),
+            Matchers.equalTo("dXJuJTNBdGVzdCUzQTM=")
+        );
+    }
+
+    /**
+     * CcBase64 can decode.
+     *
+     * @throws IOException If some problem inside
+     */
+    @Test
+    public void decodes() throws IOException {
+        MatcherAssert.assertThat(
+            new CcBase64(new CcPlain()).decode(
+                "dXJuJTNBdGVzdCUzQSVGMiVFNSVGMSVGMg=="
+                .getBytes()
+            ).urn(),
+            Matchers.equalTo("urn:test:тест")
+        );
+    }
+
+    /**
+     * CcBase64 can encode and decode.
+     *
+     * @throws IOException If some problem inside
+     */
+    @Test
+    public void encodesAndDecodes() throws IOException {
+        final String urn = "urn:test:Hello World!";
+        final Map<String, String> properties =
+            ImmutableMap.of("userName", "user");
+        final Identity identity = new Identity.Simple(urn, properties);
+        final Codec codec = new CcBase64(new CcPlain());
+        MatcherAssert.assertThat(
+            codec.decode(codec.encode(identity)).urn(),
+            Matchers.equalTo(urn)
+        );
+        MatcherAssert.assertThat(
+            codec.decode(codec.encode(identity)).properties(),
+            Matchers.equalTo(properties)
+        );
+    }
+
+    /**
+     * CcBase64 can decode invalid data.
+     *
+     * @throws IOException If some problem inside
+     */
+    @Test
+    public void decodesNonBaseSixtyFourAlphabetSymbols() throws IOException {
+        MatcherAssert.assertThat(
+            new CcSafe(new CcBase64(new CcPlain())).decode(
+                " ^^^".getBytes()
+            ).urn(),
+            Matchers.equalTo("")
+        );
+    }
+
+    /**
+     * Checks CcBase64 equals method.
+     *
+     * @throws Exception If some problem inside
+     */
+    @Test
+    public void equalsAndHashCodeEqualTest() throws Exception {
+        EqualsVerifier.forClass(CcBase64.class)
+            .suppress(Warning.TRANSIENT_FIELDS)
+            .verify();
+    }
+}
