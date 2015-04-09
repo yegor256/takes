377fe2bb380e9f7e1070df5fcd47fd140f277088
Eugene Kondrashev <eugene.kondrashev@gmail.com>
2015-04-09 08:01:20 +0300

    initial implementation

---
+++ src/main/java/org/takes/facets/fork/TkProduces.java
@@ -0,0 +1,69 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.takes.facets.fork;
+
+import java.io.IOException;
+
+import lombok.EqualsAndHashCode;
+
+import org.takes.Request;
+import org.takes.Response;
+import org.takes.Take;
+import org.takes.facets.fork.RsFork;
+import org.takes.tk.TkWrap;
+
+/**
+ * Take that compresses responses with GZIP.
+ *
+ * <p>The class is immutable and thread-safe.
+ *
+ * @author Yegor Bugayenko (yegor@teamed.io)
+ * @version $Id$
+ * @since 0.10
+ */
+@EqualsAndHashCode(callSuper = true)
+public final class TkProduces extends TkWrap {
+
+    /**
+     * Ctor.
+     * @param take Original take
+     * @param request Request
+     * @param types Accept types
+     */
+    public TkProduces(final Take take, final Request request, final String types) {
+        super(
+            new Take() {
+                @Override
+                public Response act() throws IOException {
+                    final Response response = take.act();
+                    return new RsFork(
+                        request,
+                        new FkTypes(types, response)
+                    );
+                }
+            }
+        );
+    }
+
+}
