a1f360e80a4547a006ab051d09350a0046c70da1
Dmitry Zaytsev <dmzaytsev@users.noreply.github.com>
2015-04-15 16:53:59 +0500

    #46 added cookie validation

--- src/main/java/org/takes/rs/RsWithCookie.java
+++ src/main/java/org/takes/rs/RsWithCookie.java
@@ -23,11 +23,17 @@
  */
 package org.takes.rs;
 
+import java.util.regex.Pattern;
 import lombok.EqualsAndHashCode;
 import org.takes.Response;
 
 /**
  * Response decorator, with an additional cookie.
+ *
+ * The decorator validates cookie name according
+ * @see <a href="http://tools.ietf.org/html/rfc2616#section-2.2">RFC 2616</a>
+ * and cookie value according
+ * @see <a href="http://tools.ietf.org/html/rfc6265#section-4.1.1">RFC 6265</a>
  *
  * <p>Use this decorator in order to return a response with a "Set-Cookie"
  * header inside, for example:
@@ -43,7 +49,6 @@ import org.takes.Response;
  * <pre> Set-Cookie: u=Jeff;Path=/;Expires=Wed, 13 Jan 2021 22:23:01 GMT</pre>
  *
  * <p>The class is immutable and thread-safe.
- *
  * @author Yegor Bugayenko (yegor@teamed.io)
  * @version $Id$
  * @since 0.1
@@ -74,7 +79,13 @@ public final class RsWithCookie extends RsWrap {
         final String value, final String... attrs) {
         super(
             new RsWithHeader(
-                res, "Set-Cookie", RsWithCookie.make(name, value, attrs)
+                res,
+                "Set-Cookie",
+                RsWithCookie.make(
+                    RsWithCookie.checkName(name),
+                    RsWithCookie.checkValue(value),
+                    attrs
+            )
         )
         );
     }
@@ -97,4 +108,39 @@ public final class RsWithCookie extends RsWrap {
         return text.toString();
     }
 
+    /**
+     * Checks value according RFC 6265 section 4.1.1.
+     * @param value Cookie value
+     * @return Cookie value
+     */
+    private static String checkValue(final String value) {
+        // @checkstyle LineLengthCheck (1 line)
+        if (!Pattern.matches("[\\x21\\x23-\\x2B\\x2D-\\x3A\\x3C-\\x5B\\x5D-\\x7E]*|\"[\\x21\\x23-\\x2B\\x2D-\\x3A\\x3C-\\x5B\\x5D-\\x7E]*\"", value)) {
+            throw new IllegalArgumentException(
+                String.format(
+                    "Cookie value %s contains invalid characters",
+                    value
+                )
+            );
+        }
+        return value;
+    }
+
+    /**
+     * Checks name according RFC 2616, section 2.2.
+     * @param name Cookie name;
+     * @return Cookie name
+     */
+    private static String checkName(final String name) {
+        // @checkstyle LineLengthCheck (1 line)
+        if (!Pattern.matches("[\\x20-\\x7E&&[^()<>@,;:\\\"/\\[\\]?={} ]]+", name)) {
+            throw new IllegalArgumentException(
+                String.format(
+                    "Cookie name %s contains invalid characters",
+                    name
+                )
+            );
+        }
+        return name;
+    }
 }
