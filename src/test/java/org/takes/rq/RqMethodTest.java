ff2657b3da9c9c405ea02ecec52d3edb480beaa3
Dmitry Zaystev <dmitry.zaytsev@gmail.com>
2015-04-16 00:57:26 +0500

    #137 changed constant name

--- src/test/java/org/takes/rq/RqMethodTest.java
+++ src/test/java/org/takes/rq/RqMethodTest.java
@@ -43,8 +43,8 @@ public final class RqMethodTest {
     @Test
     public void returnsMethod() throws IOException {
         MatcherAssert.assertThat(
-            new RqMethod.Base(new RqFake(RqMethod.Base.POST)).method(),
-            Matchers.equalTo(RqMethod.Base.POST)
+            new RqMethod.Base(new RqFake(RqMethod.POST)).method(),
+            Matchers.equalTo(RqMethod.POST)
         );
     }
 }
