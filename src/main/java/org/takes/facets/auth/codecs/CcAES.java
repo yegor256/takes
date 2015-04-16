d027eacd686a7fd6531cb8ef45628ed928c3702e
Jason Wong <jason-kc.wong@aia.com>
2015-04-16 16:33:19 +0800

    CcAES implementation with unit test

---
+++ src/main/java/org/takes/facets/auth/codecs/CcAES.java
@@ -0,0 +1,140 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.takes.facets.auth.codecs;
+
+import java.io.IOException;
+import java.security.SecureRandom;
+import java.security.spec.AlgorithmParameterSpec;
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import lombok.EqualsAndHashCode;
+import org.takes.facets.auth.Identity;
+
+/**
+ * AES codec which support 128 bits key.
+ *
+ * <p>The class is immutable and thread-safe.
+ *
+ * @author Jason Wong (super132j@yahoo.com)
+ * @version $Id$
+ * @since 0.13.8
+ */
+@EqualsAndHashCode(of = { "origin", "secret", "ivbytes" })
+public final class CcAES implements Codec {
+    
+    /**
+     * The algorithm used.
+     */
+    private static final transient String ALGORITHM = "AES/CBC/PKCS5PADDING";
+
+    /**
+     * Default block size (128 bits) in bytes
+     */
+    private static final transient int BLOCK = 16; 
+    
+    /**
+     * Original codec.
+     */
+    private final transient Codec origin;
+
+    /**
+     * Secret to use for encoding.
+     */
+    private final transient byte[] secret;
+
+    /**
+     * Initialization Vector.
+     */
+    private final transient byte[] ivbytes;
+    
+    /**
+     * The AES secret key object
+     */
+    private final SecretKey passcode;
+    
+    /**
+     * Ctor.
+     * @param codec Original codec
+     * @param key The encryption key
+     */
+    public CcAES(Codec codec, byte[] key) {
+        if (key.length != BLOCK) {
+            throw new IllegalArgumentException("Key length must be in 128-bit.");
+        }
+        this.origin = codec;
+        this.secret = new byte[key.length];
+        System.arraycopy(key, 0, this.secret, 0, key.length);
+        final SecureRandom random = new SecureRandom();
+        this.ivbytes = new byte[BLOCK];
+        random.nextBytes(this.ivbytes);
+        this.passcode = new SecretKeySpec(this.secret, "AES");  
+    }
+
+    @Override
+    public byte[] encode(Identity identity) throws IOException {
+        return this.encrypt(this.origin.encode(identity));
+    }
+
+    @Override
+    public Identity decode(byte[] bytes) throws IOException {
+        return this.origin.decode(this.decrypt(bytes));
+    }
+    
+    /**
+     * Encrypt the given bytes using AES
+     *
+     * @param bytes Bytes to encrypt
+     * @return Encrypted byte using AES algorithm
+     */
+    private byte[] encrypt(byte[] bytes) throws IOException {
+        try {
+            final Cipher cipher = Cipher.getInstance(ALGORITHM);
+            final AlgorithmParameterSpec spec = new IvParameterSpec(this.ivbytes);
+            cipher.init(Cipher.ENCRYPT_MODE, this.passcode, spec);
+            return cipher.doFinal(bytes);
+        } catch (Exception e) {
+            throw new IOException(e);
+        }
+    }
+
+    /**
+     * Decrypt the given bytes using AES
+     *
+     * @param bytes Bytes to decrypt
+     * @return Decrypted bytes
+     */
+    private byte[] decrypt(byte[] bytes) throws IOException {
+        try {
+            final Cipher cipher = Cipher.getInstance(ALGORITHM);
+            final AlgorithmParameterSpec spec = new IvParameterSpec(this.ivbytes);
+            cipher.init(Cipher.DECRYPT_MODE, this.passcode, spec);
+            return cipher.doFinal(bytes);
+        } catch (Exception e) {
+            throw new IOException(e);
+        }
+    }
+
+}
---
+++ src/test/java/org/takes/facets/auth/codecs/CcAESTest.java
@@ -0,0 +1,69 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.takes.facets.auth.codecs;
+
+import java.io.IOException;
+import javax.crypto.KeyGenerator;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.takes.facets.auth.Identity;
+
+/**
+ * Test case for {@link CcAES}.
+ * @author Jason Wong (super132j@yahoo.com)
+ * @version $Id$
+ * @since 0.13.8
+ */
+public final class CcAESTest {
+    
+    /**
+     * Testing AES encryption and decryption.
+     * @throws Exception any unexpected exception to throw
+     */
+    @Test
+    public void encryptAndDecrypt() throws Exception {
+        final KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(128);
+        final byte[] key = keyGen.generateKey().getEncoded();
+        final String plain = "This is a test!!@@**";
+        final Codec codec = new CcAES(
+            new Codec() {
+                @Override
+                public Identity decode(final byte[] bytes) throws IOException {
+                    return new Identity.Simple(new String(bytes));
+                }
+                @Override
+                public byte[] encode(final Identity identity) throws IOException {
+                    return identity.urn().getBytes();
+                }
+            },
+            key
+        );
+        MatcherAssert.assertThat(
+            codec.decode(codec.encode(new Identity.Simple(plain))).urn(),
+            Matchers.equalTo(plain)
+        );
+    }
+}
