8341a858634480872600b69bbb07335f27d02d31
ikhvostenkov <ikhvostenkov@gmail.com>
2015-04-30 02:27:03 +0200

    TkConsumes, TsConsumes #107 - merged and modified code due to upstream changes

--- src/main/java/org/takes/facets/fork/FkContentType.java
+++ src/main/java/org/takes/facets/fork/FkContentType.java
@@ -83,8 +83,8 @@ public final class FkContentType implements Fork {
     @SuppressWarnings("PMD.AvoidInstantiatingObjectsInLoops")
     private static MediaTypes getType(final Request req) throws IOException {
         MediaTypes list = new MediaTypes();
-        final Iterable<String> headers =
-            new RqHeaders(req).header("Content-Type");
+        final Iterable<String> headers = new RqHeaders.Base(req)
+            .header("Content-Type");
         for (final String hdr : headers) {
             list = list.merge(new MediaTypes(hdr));
         }
