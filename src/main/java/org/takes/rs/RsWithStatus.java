710acced95887a5b7e746575c1b57f89c1699117
super132 <super132j@yahoo.com>
2015-05-01 10:59:48 +0800

    Remarking RsWithStatus to use Concat and Select together.

--- src/main/java/org/takes/rs/RsWithStatus.java
+++ src/main/java/org/takes/rs/RsWithStatus.java
@@ -112,6 +112,8 @@ public final class RsWithStatus extends RsWrap {
                 )
             );
         }
+        //@todo #160:DEV To implement concatenation and selection with the
+        // conjunction Concat and Select class to get rid of List.add()
         final Collection<String> head = new LinkedList<String>();
         head.add(String.format("HTTP/1.1 %d %s", status, reason));
         boolean first = true;
