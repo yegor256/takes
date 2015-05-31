2aa920b6f2df905ebb34ec51bc8cf5a711fa4c9d
Ivan Inozemtsev <ivan.inozemtsev@gmail.com>
2015-05-31 14:14:12 +0600

    #317 TkReturn and RsReturn

---
+++ src/test/java/org/takes/facets/ret/TkReturnTest.java
@@ -0,0 +1,74 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.takes.facets.ret;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.takes.Take;
+import org.takes.rq.RqFake;
+import org.takes.rq.RqWithHeader;
+import org.takes.tk.TkEmpty;
+
+/**
+ * Test case for {@link TkReturn}.
+ * @author Ivan Inozemtsev (ivan.inozemtsev@gmail.com)
+ * @version $Id$
+ * @since 0.20
+ */
+public final class TkReturnTest {
+
+    /**
+     * TkReturn can redirect to return location
+     * and remove a return cookie.
+     * @throws IOException If some problem inside
+     */
+    @Test
+    public void redirectsAndRemovesCookie() throws IOException {
+        final Take take = new TkReturn(new TkEmpty());
+        final String destination = "/return/to";
+        MatcherAssert.assertThat(
+            take.act(
+                new RqWithHeader(
+                    new RqFake(),
+                    String.format(
+                        "Cookie: RsReturn=%s",
+                        URLEncoder.encode(
+                            destination,
+                            Charset.defaultCharset().name()
+                        )
+                    )
+                )
+            ).head(),
+            Matchers.contains(
+                "HTTP/1.1 303 See Other",
+                String.format("Location: %s", destination),
+                "Set-Cookie: RsReturn="
+            )
+        );
+    }
+}
