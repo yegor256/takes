0641abc3e4202e4f474e04a8a35d658828389a41
Yohann Ferreira <yohann.ferreira@orange.fr>
2015-06-10 01:18:45 +0200

    Fixed compilation following the rebase.

--- src/test/java/org/takes/rq/RqMultipartTest.java
+++ src/test/java/org/takes/rq/RqMultipartTest.java
@@ -31,6 +31,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.util.Arrays;
