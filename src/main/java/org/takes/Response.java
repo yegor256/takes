aae9505a7daf952e84575fef4d87616b55752fbe
bdragan <bozanovicdr@gmail.com>
2015-10-18 21:04:42 +0200

    #290: Wrapped lines longer than 80 characters.

--- src/main/java/org/takes/Response.java
+++ src/main/java/org/takes/Response.java
@@ -43,10 +43,10 @@ import java.io.InputStream;
  *   "Content-Type", "text/html"
  * );</pre>
  *
- * <p>The implementations of this interface may require that {@link Response#head()}
- * method has to be invoked before reading from the {@code InputStream},
- * but they must NOT require that the {@link InputStream} has to be read from
- * before the {@link Response#head()} method invocation.
+ * <p>The implementations of this interface may require that
+ * {@link Response#head()} method has to be invoked before reading from the
+ * {@code InputStream}, but they must NOT require that the {@link InputStream}
+ * has to be read from before the {@link Response#head()} method invocation.
  *
  * <p>All implementations of this interface must be immutable and thread-safe.
  *
