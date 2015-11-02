69b7afbdb248805d69e63d4b19f2b2312f12e4c6
Yegor Bugayenko <yegor@tpc2.com>
2015-11-02 12:49:20 -0800

    #420 RqOnce

---
+++ src/main/java/org/takes/rq/RqOnce.java
@@ -0,0 +1,76 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.takes.rq;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.atomic.AtomicBoolean;
+import lombok.EqualsAndHashCode;
+import org.takes.Request;
+
+/**
+ * Request decorator, to prevent multiple calls to {@code body()} method.
+ *
+ * <p>The class is immutable and thread-safe.
+ *
+ * @author Yegor Bugayenko (yegor@teamed.io)
+ * @version $Id$
+ * @since 0.36
+ */
+@EqualsAndHashCode(callSuper = true)
+public final class RqOnce extends RqWrap {
+
+    /**
+     * Ctor.
+     * @param req Original request
+     */
+    public RqOnce(final Request req) {
+        super(RqOnce.wrap(req));
+    }
+
+    /**
+     * Wrap the request.
+     * @param req Request
+     * @return New request
+     */
+    private static Request wrap(final Request req) {
+        final AtomicBoolean seen = new AtomicBoolean(false);
+        return new Request() {
+            @Override
+            public Iterable<String> head() throws IOException {
+                return req.head();
+            }
+            @Override
+            public InputStream body() throws IOException {
+                if (!seen.getAndSet(true)) {
+                    throw new IllegalStateException(
+                        "It's not allowed to call body() more than once"
+                    );
+                }
+                return req.body();
+            }
+        };
+    }
+
+}
---
+++ src/main/java/org/takes/tk/TkOnce.java
@@ -0,0 +1,62 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.takes.tk;
+
+import java.io.IOException;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.takes.Request;
+import org.takes.Response;
+import org.takes.Take;
+import org.takes.rq.RqGreedy;
+
+/**
+ * Take with a read-only-once request.
+ *
+ * <p>The class is immutable and thread-safe.
+ *
+ * @author Yegor Bugayenko (yegor@teamed.io)
+ * @version $Id$
+ * @since 0.26
+ */
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public final class TkOnce extends TkWrap {
+
+    /**
+     * Ctor.
+     * @param take Original take
+     */
+    public TkOnce(final Take take) {
+        super(
+            new Take() {
+                @Override
+                public Response act(final Request request) throws IOException {
+                    return take.act(new RqGreedy(request));
+                }
+            }
+        );
+    }
+
+}
