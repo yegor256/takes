445714ba39d99a5684c20ef3a3108368576cf6c0
HDouss <douss.hamdi@gmail.com>
2015-12-31 17:11:13 +0100

    TkRetry #430

---
+++ src/main/java/org/takes/tk/TkRetry.java
@@ -0,0 +1,104 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.takes.tk;
+
+import java.io.IOException;
+import org.takes.Request;
+import org.takes.Response;
+import org.takes.Take;
+
+/**
+ * Decorator TkRetry, which will not fail immediately on IOException, but
+ * will retry a few times.
+ *
+ * @author Hamdi Douss (douss.hamdi@gmail.com)
+ * @version $Id$
+ * @since 0.28.3
+ */
+public final class TkRetry implements Take {
+
+    /**
+     * Maximum number of retry attempts.
+     */
+    private final transient Integer count;
+    /**
+     * Amount of time between retries, in milliseconds.
+     */
+    private final transient Integer delay;
+
+    /**
+     * Original Take.
+     */
+    private final transient Take take;
+
+    /**
+     * Constructor.
+     *
+     * @param retry Number of retry attempts
+     * @param retrydelay Time between retries
+     * @param originaltake Original take
+     */
+    public TkRetry(final Integer retry, final Integer
+            retrydelay, final Take originaltake) {
+        this.count = retry;
+        this.delay = retrydelay;
+        this.take = originaltake;
+    }
+
+    /**
+     * Added retrying logic.
+     *
+     * @param req Request to process
+     * @return
+     * @throws IOException
+     */
+    @Override
+    public Response act(final Request req) throws IOException {
+        int attempts = 0;
+        IOException exeption = new IOException();
+        while (attempts < this.count) {
+            try {
+                return this.take.act(req);
+            } catch (final IOException ex) {
+                attempts = attempts + 1;
+                exeption = ex;
+                this.sleep();
+            }
+        }
+        throw exeption;
+    }
+
+    /**
+     * Sleep.
+     */
+    private void sleep() {
+        try {
+            Thread.sleep(this.delay);
+        } catch (final InterruptedException ex) {
+            Thread.currentThread().interrupt();
+            throw new IllegalStateException(ex);
+        }
+    }
+
+}
---
+++ src/test/java/org/takes/tk/TkRetryTest.java
@@ -0,0 +1,122 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2015 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.takes.tk;
+
+import java.io.IOException;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.takes.Request;
+import org.takes.Take;
+import org.takes.rq.RqFake;
+import org.takes.rq.RqMethod;
+import org.takes.rs.RsPrint;
+import org.takes.rs.RsText;
+
+/**
+ * TkRetry can retry till success or retry count is reached.
+ *
+ * @author Hamdi Douss (douss.hamdi@gmail.com)
+ * @version $Id$
+ * @since 0.28.3
+ */
+public final class TkRetryTest {
+
+    /**
+     * TkRetry can work when no IOException.
+     *
+     * @throws Exception if something is wrong
+     */
+    @Test
+    public void worksWithNoException() throws Exception {
+        final String test = "test";
+        MatcherAssert.assertThat(
+                new RsPrint(
+                        new TkRetry(2, 2, new TkText(test))
+                                .act(new RqFake())
+                ).print(),
+                Matchers.containsString(test)
+        );
+    }
+
+    /**
+     * TkRetry can retry when initial take fails with IOException,
+     * till retry count is reached.
+     *
+     * @throws Exception if something is wrong
+     */
+    @Test(expected = IOException.class)
+    public void retriesOnExceptionTillCount() throws Exception {
+        final int count = 3;
+        final int delay = 1000;
+        final Take take = Mockito.mock(Take.class);
+        final Request req = new RqFake(RqMethod.GET);
+        Mockito.when(take.act(Mockito.any(Request.class))).thenThrow(
+                new IOException()
+        );
+        final long startTime = System.currentTimeMillis();
+        try {
+            new TkRetry(count, delay, take).act(req);
+        } catch (final IOException exception) {
+            MatcherAssert.assertThat(
+                new Long(count * delay),
+                Matchers.lessThanOrEqualTo(
+                        System.currentTimeMillis() - startTime
+                )
+            );
+            throw exception;
+        }
+    }
+
+    /**
+     * TkRetry can retry when initial take fails with IOException,
+     * till get successful result.
+     *
+     * @throws Exception if something is wrong
+     */
+    @Test
+    public void retriesOnExceptionTillSuccess() throws Exception {
+        final int count = 3;
+        final int delay = 1000;
+        final String data = "data";
+        final Take take = Mockito.mock(Take.class);
+        Mockito.when(take.act(Mockito.any(Request.class))).thenThrow(
+                new IOException()
+        ).thenReturn(new RsText(data));
+        final long startTime = System.currentTimeMillis();
+        final RsPrint response = new RsPrint(
+            new TkRetry(count, delay, take).act(new RqFake(RqMethod.GET))
+        );
+        MatcherAssert.assertThat(
+            new Long(delay),
+            Matchers.lessThanOrEqualTo(System.currentTimeMillis() - startTime)
+        );
+        MatcherAssert.assertThat(
+            response.print(),
+            Matchers.containsString(data)
+        );
+    }
+}
