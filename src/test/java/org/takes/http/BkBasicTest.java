70f5e58a66b0a83752482745a0c158e1cc3ee599
Maksimenko Vladimir <xupypr@xupypr.com>
2016-01-07 22:00:10 +0300

    checkstyle mods

--- src/test/java/org/takes/http/BkBasicTest.java
+++ src/test/java/org/takes/http/BkBasicTest.java
@@ -57,6 +57,21 @@ import org.takes.tk.TkText;
  */
 public final class BkBasicTest {
     /**
+     * Carriage return constant.
+     */
+    private static final String CRLF = "\r\n";
+
+    /**
+     * POST header constant.
+     */
+    private static final String POST = "POST / HTTP/1.1";
+
+    /**
+     * Host header constant.
+     */
+    private static final String HOST = "Host:localhost";
+
+    /**
      * BkBasic can handle socket data.
      * @throws IOException If some problem inside
      */
@@ -65,7 +80,7 @@ public final class BkBasicTest {
         final Socket socket = Mockito.mock(Socket.class);
         Mockito.when(socket.getInputStream()).thenReturn(
             new ByteArrayInputStream(
-                Joiner.on("\r\n").join(
+                Joiner.on(BkBasicTest.CRLF).join(
                     "GET / HTTP/1.1",
                     "Host:localhost",
                     "Content-Length: 2",
@@ -124,9 +139,9 @@ public final class BkBasicTest {
         final String text = "Hello world!";
         final MkContainer container = new MkGrizzlyContainer().next(
             new MkAnswer.Simple(
-                Joiner.on("\r\n").join(
-                    "POST / HTTP/1.1",
-                    "Host: localhost",
+                Joiner.on(BkBasicTest.CRLF).join(
+                    POST,
+                    HOST,
                     "Content-Length: 4",
                     "",
                     "hi",
@@ -159,9 +174,9 @@ public final class BkBasicTest {
     public void returnsProperResponseCodeOnNoContentLength() throws Exception {
         final MkContainer container = new MkGrizzlyContainer().next(
             new MkAnswer.Simple(
-                Joiner.on("\r\n").join(
-                    "POST / HTTP/1.1",
-                    "Host: localhost",
+                Joiner.on(BkBasicTest.CRLF).join(
+                    POST,
+                    HOST,
                     "",
                     "hi"
                 )
@@ -187,9 +202,9 @@ public final class BkBasicTest {
         final String text = "Close Test";
         final MkContainer container = new MkGrizzlyContainer().next(
             new MkAnswer.Simple(
-                Joiner.on("\r\n").join(
-                    "POST / HTTP/1.1",
-                    "Host: localhost",
+                Joiner.on(BkBasicTest.CRLF).join(
+                    POST,
+                    HOST,
                     "Connection: Close",
                     "",
                     "hi"
