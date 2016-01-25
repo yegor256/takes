10bfff9f24921521659a92c392dc80e1d57cc02b
rfqu <rfq@list.ru>
2016-01-26 00:49:28 +0300

    some checkstyle errors fixed.

--- src/main/java/org/takes/facets/hamcrest/HmRsBody.java
+++ src/main/java/org/takes/facets/hamcrest/HmRsBody.java
@@ -30,7 +30,6 @@ import java.nio.charset.Charset;
 import java.nio.charset.IllegalCharsetNameException;
 import java.util.Iterator;
 import java.util.regex.Pattern;
-
 import org.hamcrest.Description;
 import org.hamcrest.TypeSafeMatcher;
 import org.takes.Response;
@@ -41,49 +40,62 @@ import org.takes.Response;
  * <p>This "matcher" tests given response body.
  * <p>The class is immutable and thread-safe.
  *
+ * @version
  * @author Alexei Kaigorodov (alexei.kaigorodov@gmail.com)
  */
 public final class HmRsBody extends TypeSafeMatcher<Response> {
 
-	/**
-	 * \ Description message.
-	 */
-    private static final Pattern patt = Pattern.compile("^\\s*charset\\s*=\\s*(\\w*)\\s*$");  
+    /**
+     * Pattern to extract charset name.
+     */
+    private static final Pattern PATT =
+        Pattern.compile("^\\s*charset\\s*=\\s*(\\w*)\\s*$");
 
+    /**
+     * Body value as byte array.
+     */
     private byte[] value;
+    
+    /**
+     * Body value as String.
+     */
     private String stringValue;
+    
+    /**
+     * encoding of the body byte array.
+     */
     private Charset charset;
     
-	public HmRsBody(String str, String charsetName) {
-		this(str, Charset.forName(charsetName));
-	}
+    public HmRsBody(String str, String charsetName) {
+        this(str, Charset.forName(charsetName));
+    }
 
     public HmRsBody(String str) {
-		this (str, (Charset)null);
+        this (str, (Charset)null);
    }
 
-	public HmRsBody(String str, Charset charset) {
-		if (str==null) {
-			throw new IllegalArgumentException("str may not be null");
-		}
-		this.stringValue=str;
-		if (charset!=null) {
-			value=str.getBytes(charset);
-			this.charset=charset;
-		}
+    public HmRsBody(String str, Charset charset) {
+        if (str==null) {
+            throw new IllegalArgumentException("str may not be null");
+        }
+        this.stringValue=str;
+        if (charset!=null) {
+            value=str.getBytes(charset);
+            this.charset=charset;
+        }
     }
 
-	public HmRsBody(byte[] val) {
-		if (val==null) {
-			throw new IllegalArgumentException("byte array may not be null");
-		}
-		value=val;
+    public HmRsBody(byte[] val) {
+        if (val==null) {
+            throw new IllegalArgumentException("byte array may not be null");
+        }
+        value=val;
     }
 
-	@Override
+    @Override
     public void describeTo(final Description description) {
         if (stringValue==null) {
-        	stringValue=new String(value, charset);
+            stringValue=new String(value, charset);
         }
         description.appendText("body: ")
             .appendText(stringValue);
@@ -91,77 +103,77 @@ public final class HmRsBody extends TypeSafeMatcher<Response> {
 
     @Override
     public boolean matchesSafely(final Response response) {
-		try {
-			Iterable<String> head = response.head();
-			if (charset==null) {
-				// try to extract charset from header
-				Iterator<String> it = head.iterator();
-				while (it.hasNext()) {
-					java.util.regex.Matcher strMatcher = patt.matcher(it.next());
-					if (strMatcher.find()) {
-						try {
-							charset=Charset.forName(strMatcher.group());
-						} catch (IllegalCharsetNameException e) {
-						}
-						break;
-					}
-				}
-			}
+        try {
+            Iterable<String> head = response.head();
+            if (charset==null) {
+                // try to extract charset from header
+                Iterator<String> it = head.iterator();
+                while (it.hasNext()) {
+                    java.util.regex.Matcher strMatcher = PATT.matcher(it.next());
+                    if (strMatcher.find()) {
+                        try {
+                            charset=Charset.forName(strMatcher.group());
+                        } catch (IllegalCharsetNameException e) {
+                        }
+                        break;
+                    }
+                }
+            }
 
-			InputStream body=null;
-			try {
-				body = response.body();
-				if (value!=null) {
-					if (value.length == 0) {
-						return (body.read()==-1);
-					}
-					byte[] buf = new byte[Math.min(value.length, 4096)];
-					for (int total=0; ; ) {
-						int rd=body.read(buf);
-					    if (rd == -1) {
-		        			return total == value.length;
-					    }
-					    for (int k=0; k<rd; k++) {
-					    	if (buf[k]!=value[total+k]) {
-					    		return false;
-					    	}
-					    }
-					    total+=rd;
-					}
-				} else if (stringValue!=null) {
-					InputStreamReader reader;
-					if (charset!=null) {
-						reader=new InputStreamReader(body, charset);
-					} else {
-					    reader=new InputStreamReader(body);
-					}
-					if (stringValue.length() == 0) {
-						return (reader.read()==-1);
-					}
-					char[] buf = new char[Math.min(stringValue.length(), 2048)];
-					for (int total=0; ; ) {
-						int rd=reader.read(buf);
-					    if (rd == -1) {
-		        			return total == stringValue.length();
-					    }
-					    for (int k=0; k<rd; k++) {
-					    	if (buf[k]!=stringValue.charAt(total+k)) {
-					    		return false;
-					    	}
-					    }
-					    total+=rd;
-					}
-				} else {
-					throw new IllegalStateException("both stribg and byte array are null");
-				}
-			} finally {
-				if (body!=null) {
-				    body.close();
-				}
-		    }
-		} catch (final IOException ex) {
+            InputStream body=null;
+            try {
+                body = response.body();
+                if (value!=null) {
+                    if (value.length == 0) {
+                        return (body.read()==-1);
+                    }
+                    byte[] buf = new byte[Math.min(value.length, 4096)];
+                    for (int total=0; ; ) {
+                        int rd=body.read(buf);
+                        if (rd == -1) {
+                            return total == value.length;
+                        }
+                        for (int k=0; k<rd; k++) {
+                            if (buf[k]!=value[total+k]) {
+                                return false;
+                            }
+                        }
+                        total+=rd;
+                    }
+                } else if (stringValue!=null) {
+                    InputStreamReader reader;
+                    if (charset!=null) {
+                        reader=new InputStreamReader(body, charset);
+                    } else {
+                        reader=new InputStreamReader(body);
+                    }
+                    if (stringValue.length() == 0) {
+                        return (reader.read()==-1);
+                    }
+                    char[] buf = new char[Math.min(stringValue.length(), 2048)];
+                    for (int total=0; ; ) {
+                        int rd=reader.read(buf);
+                        if (rd == -1) {
+                            return total == stringValue.length();
+                        }
+                        for (int k=0; k<rd; k++) {
+                            if (buf[k]!=stringValue.charAt(total+k)) {
+                                return false;
+                            }
+                        }
+                        total+=rd;
+                    }
+                } else {
+                    throw new IllegalStateException("both stribg and byte array are null");
+                }
+            } finally {
+                if (body!=null) {
+                    body.close();
+                }
+            }
+        } catch (final IOException ex) {
             throw new IllegalStateException(ex);
-		}
+        }
     }
 
     @Override
