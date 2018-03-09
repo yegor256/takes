01ab54de59924b73a2e4ea23de7dbc192aeee63d
Yegor Bugayenko <yegor256@gmail.com>
2018-03-09 15:35:03 +0300

    #823 TkSslOnly

---
+++ src/main/java/org/takes/tk/TkSslOnly.java
@@ -0,0 +1,80 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2018 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.takes.tk;
+
+import java.io.IOException;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.takes.Request;
+import org.takes.Response;
+import org.takes.Take;
+import org.takes.rq.RqHeaders;
+import org.takes.rq.RqHref;
+import org.takes.rs.RsRedirect;
+
+/**
+ * Take that redirects to HTTPS if it's HTTP.
+ *
+ * <p>The class is immutable and thread-safe.
+ *
+ * @author Yegor Bugayenko (yegor256@gmail.com)
+ * @version $Id$
+ * @since 1.9
+ */
+@ToString
+@EqualsAndHashCode
+public final class TkSslOnly implements Take {
+
+    /**
+     * Original take.
+     */
+    private final Take origin;
+
+    /**
+     * Ctor.
+     * @param take Original take
+     */
+    public TkSslOnly(final Take take) {
+        this.origin = take;
+    }
+
+    @Override
+    @SuppressWarnings("PMD.AvoidDuplicateLiterals")
+    public Response act(final Request req) throws IOException {
+        final String href = new RqHref.Base(req).href().toString();
+        final String proto = new RqHeaders.Smart(
+            new RqHeaders.Base(req)
+        ).single("x-forwarded-proto", "https");
+        final Response answer;
+        if ("https".equalsIgnoreCase(proto)) {
+            answer = this.origin.act(req);
+        } else {
+            answer = new RsRedirect(
+                href.replaceAll("^http", "https")
+            );
+        }
+        return answer;
+    }
+
+}
---
+++ src/test/java/org/takes/tk/TkSslOnlyTest.java
@@ -0,0 +1,79 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2018 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.takes.tk;
+
+import java.io.IOException;
+import java.util.Arrays;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.Test;
+import org.takes.Request;
+import org.takes.Response;
+import org.takes.Take;
+import org.takes.rq.RqFake;
+import org.takes.rq.RqPrint;
+import org.takes.rs.RsPrint;
+import org.takes.rs.RsText;
+
+/**
+ * Test case for {@link TkSslOnly}.
+ * @author Yegor Bugayenko (yegor256@gmail.com)
+ * @version $Id$
+ * @since 1.9
+ */
+public final class TkSslOnlyTest {
+
+    /**
+     * Redirects when it's HTTP instead of HTTPS.
+     * @throws Exception If fails
+     */
+    @Test
+    public void redirects() throws Exception {
+        final Request req = new RqFake(
+            Arrays.asList(
+                "GET /one/two?a=1",
+                "Host: www.0crat.com",
+                "X-Forwarded-Proto: http"
+            ),
+            ""
+        );
+        MatcherAssert.assertThat(
+            new RsPrint(
+                new TkSslOnly(
+                    new Take() {
+                        @Override
+                        public Response act(final Request request)
+                            throws IOException {
+                            return new RsText(
+                                new RqPrint(request).print()
+                            );
+                        }
+                    }
+                ).act(req)
+            ).print(),
+            Matchers.containsString("https://www.0crat.com/one/two?a=1")
+        );
+    }
+
+}
