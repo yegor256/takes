2b114d3e90cf400a8a6937294d4d0d7903d43aab
Paulo Lobo <pauloeduardolobo@gmail.com>
2018-09-04 00:21:15 -0300

    For #791: - Created Expires interface and default implementations - Added Tests for Expire implementations in ExpiresTest

--- src/main/java/org/takes/misc/Expires.java
+++ src/main/java/org/takes/misc/Expires.java
@@ -1,15 +1,44 @@
+/**
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2018 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package org.takes.misc;
-
 
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.util.TimeZone;
 
+/**
+ * Expiration date in GMT.
+ *
+ * @author Paulo Lobo (pauloeduardolobo@gmail.com)
+ * @version $Id$
+ * @since 2.0
+ */
 public interface Expires {
 
     /**
      * String representation of expiration time.
-     * @return
+     * @return Representation of expirate time.
      */
     String print();
 
@@ -17,9 +46,22 @@ public interface Expires {
      * Never expires.
      */
     final class Never implements Expires {
+
+        /**
+         * Original time.
+         */
+        private final Expires origin;
+
+        /**
+         * Constructor.
+         */
+        Never() {
+            this.origin = new Date(0L);
+        }
+
         @Override
         public String print() {
-            throw new UnsupportedOperationException("print() not implemented");
+            return this.origin.print();
         }
     }
 
@@ -33,21 +75,25 @@ public interface Expires {
          */
         private final Expires origin;
 
+        /**
+         * Constructor.
+         * @param origin Original time
+         */
         Hour(final Expires origin) {
             this.origin = origin;
         }
 
         @Override
         public String print() {
-            throw new UnsupportedOperationException("print() not implemented");
+            return this.origin.print();
         }
     }
 
     /**
      * Expiration date in GMT.
      *
-     * @author Paulo Lobo (pauloeduardolobo@gmail.com)
-     * @version $$
+     * @author Izbassar Tolegen (t.izbassar@gmail.com)
+     * @version $Id$
      * @since 2.0
      */
     final class Date implements Expires {
@@ -88,7 +134,7 @@ public interface Expires {
          * @param expiration Expiration in millis
          */
         public Date(final String ptn, final Locale locale,
-                    final long expiration) {
+            final long expiration) {
             this(ptn, locale, new java.util.Date(expiration));
         }
 
@@ -99,7 +145,7 @@ public interface Expires {
          * @param expires Date when expires
          */
         public Date(final String ptn, final Locale locale,
-                    final java.util.Date expires) {
+            final java.util.Date expires) {
             this.format = new ThreadLocal<SimpleDateFormat>() {
                 @Override
                 protected SimpleDateFormat initialValue() {
