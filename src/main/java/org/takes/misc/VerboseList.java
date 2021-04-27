ddc81521c6ca83ae559c3817f7c5a7e41f669fe1
andreoss <andreoss@sdf.org>
2021-04-27 12:32:56 -0400

    (#1062) Don't call `toString` explicitly

--- src/main/java/org/takes/misc/VerboseList.java
+++ src/main/java/org/takes/misc/VerboseList.java
@@ -29,6 +29,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 import org.cactoos.Text;
+import org.cactoos.text.UncheckedText;
 
 /**
  * Verbose List that wraps OutOfBoundsException with custom message.
@@ -211,7 +212,9 @@ public final class VerboseList<T> implements List<T> {
     private IndexOutOfBoundsException wrapException(
         final IndexOutOfBoundsException cause) {
         final IndexOutOfBoundsException exc =
-            new IndexOutOfBoundsException(this.message);
+            new IndexOutOfBoundsException(
+                new UncheckedText(this.message).asString()
+            );
         exc.initCause(cause);
         return exc;
     }
