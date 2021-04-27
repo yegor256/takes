a2014a6e8246edf14cf7a742eeb0628eac6526e4
andreoss <andreoss@sdf.org>
2021-04-27 12:30:37 -0400

    (#1062) Don't call `toString` explicitly

--- src/main/java/org/takes/rq/form/RqFormBase.java
+++ src/main/java/org/takes/rq/form/RqFormBase.java
@@ -93,7 +93,7 @@ public final class RqFormBase extends RqWrap implements RqForm {
                 new FormattedText(
                     "there are no params \"%s\" among %d others: %s",
                     key, this.map().size(), this.map().keySet()
-                ).toString()
+                )
             );
         } else {
             iter = new VerboseIterable<>(
@@ -101,7 +101,7 @@ public final class RqFormBase extends RqWrap implements RqForm {
                 new FormattedText(
                     "there are only %d params by name \"%s\"",
                     values.size(), key
-                ).toString()
+                )
             );
         }
         return iter;
