eb270c27f12d29d7ff4242b60794d08148fe4772
Yegor Bugayenko <yegor256@gmail.com>
2022-07-11 10:55:50 +0300

    #1127 year up

--- src/main/java/org/takes/rq/RqFake.java
+++ src/main/java/org/takes/rq/RqFake.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2014-2019 Yegor Bugayenko
+ * Copyright (c) 2014-2022 Yegor Bugayenko
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -56,7 +56,7 @@ public final class RqFake extends RqWrap {
      * @param method HTTP method
      */
     public RqFake(final CharSequence method) {
-        this(method, "/ HTTP/1.1 ");
+        this(method, "/ HTTP/1.1");
     }
 
     /**
