41df45d24458acb82b4043966cc91afd8d03e25e
Yegor Bugayenko <yegor256@gmail.com>
2022-07-16 22:59:52 +0300

    #1134 extra test

---
+++ src/test/java/org/takes/rs/RsWithBodyTest.java
@@ -0,0 +1,59 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014-2022 Yegor Bugayenko
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package org.takes.rs;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.takes.Response;
+
+/**
+ * Test case for {@link RsWithBody}.
+ * @since 1.22
+ * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
+ */
+final class RsWithBodyTest {
+
+    /**
+     * RsWithType can replace an existing type.
+     * @throws IOException If fails
+     */
+    @Test
+    void buildsResponseWithBody() throws IOException {
+        final Response res = new RsWithBody(
+            new RsWithStatus(HttpURLConnection.HTTP_OK),
+            "привет, Jeff"
+        );
+        new RsPrint(res).print();
+        MatcherAssert.assertThat(
+            new RsPrint(res).print(),
+            Matchers.containsString(
+                "Content-Length: 18\r\n\r\nпривет"
+            )
+        );
+    }
+
+}
